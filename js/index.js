88d5616faf72d3092b3744278631df6605490c0f
DeAndre Johnson <dre@phreshr.com>
2019-10-01 05:23:35 +0800

    added all dom elements

--- js/index.js
+++ js/index.js
@@ -42,11 +42,78 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute("src", siteContent["nav"]["img-src"]);
 logo.setAttribute("alt", "Great Idea! Company logo");
 
+// Nav
 const navLinks = document.querySelectorAll('nav a')
-const navText = Object.values(siteContent.nav).map(element => element)
+const linkText = Object.values(siteContent["nav"]).map(element => element)
+const linkClass = Object.keys(siteContent["nav"]).map(element => element)
 
 Array.from(navLinks).map((link, i) => {
-  link.classList.add(`nav-item-${i + 1}`)
-  link.textContent = navText[i]
+  link.classList.add(linkClass[i])
+  link.textContent = linkText[i]
 })
 
+// CTA
+const ctaH1 = document.querySelector('.cta-text h1')
+ctaH1.textContent = siteContent["cta"]["h1"]
+
+const h1BR = ctaH1.textContent.split(" ");
+ctaH1.innerHTML = h1BR.join("</BR>");
+
+const ctaBTN = document.querySelector('.cta-text button')
+ctaBTN.textContent = siteContent["cta"]["button"]
+
+const ctaIMG = document.querySelector('#cta-img')
+ctaIMG.src = siteContent["cta"]["img-src"];
+
+// Top Content
+const topContent = document.querySelector('.top-content')
+const featuresH4 = topContent.querySelectorAll('.text-content h4')[0]
+const aboutH4 = topContent.querySelectorAll('.text-content h4')[1]
+featuresH4.textContent = siteContent["main-content"]["features-h4"]
+aboutH4.textContent = siteContent["main-content"]["about-h4"]
+
+const featuresContent = topContent.querySelectorAll('.text-content p')[0]
+const aboutContent = topContent.querySelectorAll('.text-content p')[1]
+
+featuresContent.textContent = siteContent["main-content"]["features-content"]
+aboutContent.textContent = siteContent["main-content"]["about-content"]
+
+// Middle Img
+const middleIMG = document.querySelector('.middle-img')
+middleIMG.src = siteContent["main-content"]["middle-img-src"]
+
+// Bottom Content
+const bottomContent = document.querySelector('.bottom-content')
+const servicesH4 = bottomContent.querySelectorAll('.text-content h4')[0]
+const productH4 = bottomContent.querySelectorAll('.text-content h4')[1]
+const visionH4 = bottomContent.querySelectorAll('.text-content h4')[2]
+servicesH4.textContent = siteContent["main-content"]["services-h4"]
+productH4.textContent = siteContent["main-content"]["product-h4"]
+visionH4.textContent = siteContent["main-content"]["vision-h4"]
+
+const servicesContent = bottomContent.querySelectorAll('.text-content p')[0]
+const productContent = bottomContent.querySelectorAll('.text-content p')[1]
+const visionContent = bottomContent.querySelectorAll('.text-content p')[2]
+
+servicesContent.textContent = siteContent["main-content"]["services-content"]
+productContent.textContent = siteContent["main-content"]["product-content"]
+visionContent.textContent = siteContent["main-content"]["vision-content"]
+
+// Contact
+const contact = document.querySelector('.contact')
+const contactH4 = contact.querySelector('h4')
+contactH4.textContent = siteContent["contact"]["contact-h4"]
+
+const address = contact.querySelectorAll('p')[0]
+const phone = contact.querySelectorAll('p')[1]
+const email = contact.querySelectorAll('p')[2]
+address.textContent = siteContent["contact"]["address"]
+phone.textContent = siteContent["contact"]["phone"]
+email.textContent = siteContent["contact"]["email"]
+
+const addressBR = address.textContent.split("Street");
+address.innerHTML = addressBR.join("Street </BR>");
+
+// Footer
+let footerText = document.querySelector('footer p');
+footerText.textContent = siteContent["footer"]["copyright"];
